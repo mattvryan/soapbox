c2430176a3165a4c704095a8e1a4b3028ddee361
Matt Ryan <matt@mvryan.org>
2015-06-19 16:46:07 -0600

    Capture dropdown toggle info in a single function.

--- app/js/soapbox.js
+++ app/js/soapbox.js
@@ -13,68 +13,36 @@
 function createToolbar() {
     $('#sb_menu').hide();
     $('#sb_menu_toolbar_icon').click( function() {
-        var wasDisplayed = $('#sb_menu:visible').length;
-        $('.sb_dropdown').fadeOut();
-        if (wasDisplayed) {
-            $('#sb_menu').fadeOut();
-        } else {
-            $('#sb_menu').slideDown();
-        }
+        return toolbar_toggle('#sb_menu');
     });
     $('#sb_search').hide();
     $('#sb_search_toolbar_icon').click( function() {
-        var wasDisplayed = $('#sb_search:visible').length;
-        $('.sb_dropdown').fadeOut();
-        if (wasDisplayed) {
-            $('#sb_search').fadeOut();
-        } else {
-            $('#sb_search').slideDown();
+        return toolbar_toggle('#sb_search', function() {
             $('#sb_search_input').focus();
-        }
+        });
     });
     $('#sb_login').hide();
     $('#sb_login_toolbar_icon').click( function() {
-        var wasDisplayed = $('#sb_login:visible').length;
-        $('.sb_dropdown').fadeOut();
-        if (wasDisplayed) {
-            $('#sb_login').fadeOut();
-        } else {
-            $('#sb_login').slideDown();
+        return toolbar_toggle('#sb_login', function () {
             $('#sb_login_username_input').focus();
             $('#sb_login_submit_button').attr('disabled', true);
-        }
+        });
     });
     $('#sb_logout_toolbar_icon').hide().click( function() {
-        var wasDisplayed = $('#sb_logout:visible').length;
-        $('.sb_dropdown').fadeOut();
-        if (wasDisplayed) {
-            $('#sb_logout').fadeOut();
-        } else {
-            $('#sb_logout').slideDown();
-        }
+        return toolbar_toggle('#sb_logout');
     });
     $('#sb_login_submit_button').click( function(e) {
         e.preventDefault();
         // change the submit button to a washing machine
-        var username_input = $('#sb_login_username_input');
-        var password_input = $('#sb_login_password_input');
-        var login_result = login(username_input.val(), password_input.val());
+        var login_result = login($('#sb_login_username_input').val(), $('#sb_login_password_input').val(), onLoginSucceeded, onLoginFailed);
         $('#sb_login').fadeOut();
-        username_input.val('');
-        password_input.val('');
-        if (login_result) {
-            $('#sb_login_toolbar_icon').hide();
-            $('#sb_logout_toolbar_icon').show();
-        } else {
-            alert('Invalid login credential supplied.');
-        }
     });
     $('#sb_logout').hide().click( function(e) {
         e.preventDefault();
         $('#sb_logout').fadeOut();
         $('#sb_logout_toolbar_icon').hide();
         $('#sb_login_toolbar_icon').show();
-        logout();
+        logout(onLogoutSucceeded, onLogoutFailed);
     });
     $('#sb_login_username_input').keyup( function() {
         $('#sb_login_submit_button').attr('disabled', $(this).val().length == 0 || $('#sb_login_password_input').val().length == 0);
@@ -84,20 +52,43 @@ function createToolbar() {
     })
 }
 
-function login(username, password) {
-    if (! username || 0 == username.length || ! password || 0 == password.length)
-    {
-        return false;
+function toolbar_toggle(tb_class_sel, onToolbarDropdownShow, onToolbarDropdownHide) {
+    var wasDisplayed = $(tb_class_sel+':visible').length;
+    $('.sb_dropdown').fadeOut();
+    if (wasDisplayed) {
+        $(tb_class_sel).fadeOut();
+        if (onToolbarDropdownHide) onToolbarDropdownHide();
+    } else {
+        $(tb_class_sel).slideDown();
+        if (onToolbarDropdownShow) onToolbarDropdownShow();
     }
+}
+
+function login(username, password, cbSuccess, cbFailure) {
     // login here
 
     // dummy login
     if (username === 'abc' && password === '123') {
-        return true;
+        return cbSuccess();
     }
-    return false;
+    return cbFailure();
 }
 
-function logout() {
+function onLoginSucceeded() {
+    // change the welcome text
+    $('#sb_login').fadeOut();
+    $('#sb_login_username_input').val('');
+    $('#sb_login_password_input').val('');
+    $('#sb_login_toolbar_icon').hide();
+    $('#sb_logout_toolbar_icon').show();
+}
+
+function onLoginFailed() {
+    // show the popup dialog
+    alert('Invalid login credential supplied.');
+    $('#sb_login').slideDown();
+}
+
+function logout(cbSuccess, cbFailure) {
     // logout here
 }
